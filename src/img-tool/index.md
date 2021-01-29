62eb221dae506d9d985b11fa5a07723509684524
dyb881 <15118995517@163.com>
2021-01-29 12:16:41 +0800

    添加网络图片转 base64 和 file

--- src/img-tool/index.md
+++ src/img-tool/index.md
@@ -30,7 +30,11 @@ getImg(src);
 
 一般情况下 Typescript 代码提示完全可以明白使用方法。如有不明的情况请跳转[函数定义](https://github.com/dyb881/seasoning/blob/master/src/img-tool/index.ts)
 
-| Function    | Description       |
-| ----------- | ----------------- |
-| getImg      | 获取图片对象      |
-| imgToBase64 | img 标签转 base64 |
+| Function     | Description            |
+| ------------ | ---------------------- |
+| getImg       | 获取图片对象           |
+| imgToBase64  | img 标签转 base64      |
+| urlToBase64  | 网络图片转 Base64      |
+| urlToBase64s | 网络图片转 Base64 批量 |
+| urlToFile    | 网络图片转文件         |
+| urlToFiles   | 网络图片转文件 批量    |
--- src/img-tool/index.ts
+++ src/img-tool/index.ts
@@ -1,3 +1,5 @@
+import { base64ToFile } from '../file-tool';
+
 /**
  * 获取图片对象
  */
@@ -12,7 +14,7 @@ export const getImg = (src: string) => {
  * img 标签转 base64
  */
 export const imgToBase64 = (img: HTMLImageElement) => {
-  return new Promise<string>(resolve => {
+  return new Promise<string>((resolve) => {
     img.onload = () => {
       const { width, height } = img;
       const canvas = document.createElement('canvas');
@@ -23,6 +25,36 @@ export const imgToBase64 = (img: HTMLImageElement) => {
       resolve(dataURL);
     };
   });
+};
+
+/**
+ * 网络图片转Base64
+ */
+export const urlToBase64 = async (src: string) => {
+  const img = await getImg(src);
+  return imgToBase64(img);
+};
+
+/**
+ * 网络图片转Base64 批量
+ */
+export const urlToBase64s = (srcs: string[]) => {
+  return Promise.all(srcs.map((src, index) => urlToBase64(src)));
+};
+
+/**
+ * 网络图片转文件
+ */
+export const urlToFile = async (src: string, fileName?: string) => {
+  const base64 = await urlToBase64(src);
+  return base64ToFile(base64, fileName);
+};
+
+/**
+ * 网络图片转文件 批量
+ */
+export const urlToFiles = (srcs: string[], fileNames = [] as string[]) => {
+  return Promise.all(srcs.map((src, index) => urlToFile(src, fileNames[index])));
 };
 
 export default { getImg, imgToBase64 };
